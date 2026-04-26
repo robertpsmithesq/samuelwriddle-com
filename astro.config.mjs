92ab0ff2c1e515d52c5fe4982220d1e305132b8b
Robert Smith <robertpsmithesq@gmail.com>
2026-04-26 14:10:55 -0500

    Add astro lint check and prettier formatter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import mdx from '@astrojs/mdx';
-import tailwindcss from '@tailwindcss/vite';
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
